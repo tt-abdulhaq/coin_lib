29d086c7206fc71b8f8df84ee139f6cd8997fd7b
Abdulhaq <119478292+tt-abdulhaq@users.noreply.github.com>
2024-03-25 13:45:45 +0430

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,33 @@
+# Import the FieldElement class
 from field_element import FieldElement
 
+# Define prime and elements
+p = 13
+a = FieldElement(7, p)
+b = FieldElement(12, p)
+c = FieldElement(6, p)
+
+print(a+b==c)
+print(f'a + b = {a+b}')
+print(f'c - b = {c-b}')
 
 
+print(a*b==c)
+print(f'a . c = {a*c}')
+print(f'b . a = {b * a}')
 
-a = FieldElement(3, 13)
-b = FieldElement(1, 13)
-c = FieldElement(10, 13)
-a//b
+t = FieldElement(7, 31)
+
+print(t**(-3))
+
+p = 19
+
+a = FieldElement(2, p)
+
+b = FieldElement(7, p)
+
+c = FieldElement(5, p)
+
+print(a/b)
+print(b/c)
+
